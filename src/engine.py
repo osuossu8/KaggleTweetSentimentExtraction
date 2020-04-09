8864788b62dfb6df33a9f063a6ec501d9079bc8e
osuossu8 <osuosuossu18@gmail.com>
2020-04-09 14:25:46 +0000

    exp3 0.694 bert public kernel

--- src/engine.py
+++ src/engine.py
@@ -7,10 +7,18 @@ from tqdm import tqdm
 import src.config as config
 
 
-def loss_fn(o1, o2, t1, t2):
-    l1 = nn.BCEWithLogitsLoss()(o1, t1)
-    l2 = nn.BCEWithLogitsLoss()(o2, t2)
-    return l1 + l2
+#def loss_fn(o1, o2, t1, t2):
+#    l1 = nn.BCEWithLogitsLoss()(o1, t1)
+#    l2 = nn.BCEWithLogitsLoss()(o2, t2)
+#    return l1 + l2
+
+
+def loss_fn(start_logits, end_logits, start_positions, end_positions):
+    loss_fct = nn.CrossEntropyLoss()
+    start_loss = loss_fct(start_logits, start_positions)
+    end_loss = loss_fct(end_logits, end_positions)
+    total_loss = (start_loss + end_loss)
+    return total_loss
 
 
 def jaccard(str1, str2): 
@@ -38,6 +46,145 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
+def calculate_jaccard_score(
+    original_tweet, 
+    target_string, 
+    sentiment_val, 
+    idx_start, 
+    idx_end, 
+    offsets,
+    verbose=False):
+    
+    if idx_end < idx_start:
+        idx_end = idx_start
+    
+    filtered_output  = ""
+    for ix in range(idx_start, idx_end + 1):
+        filtered_output += original_tweet[offsets[ix][0]: offsets[ix][1]]
+        if (ix+1) < len(offsets) and offsets[ix][1] < offsets[ix+1][0]:
+            filtered_output += " "
+
+    if sentiment_val == "neutral" or len(original_tweet.split()) < 2:
+        filtered_output = original_tweet
+
+    jac = jaccard(target_string.strip(), filtered_output.strip())
+    return jac, filtered_output
+
+
+def train_fn(data_loader, model, optimizer, device, scheduler=None):
+    model.train()
+    losses = AverageMeter()
+    jaccards = AverageMeter()
+
+    tk0 = tqdm(data_loader, total=len(data_loader))
+    
+    for bi, d in enumerate(tk0):
+
+        ids = d["ids"]
+        token_type_ids = d["token_type_ids"]
+        mask = d["mask"]
+        targets_start = d["targets_start"]
+        targets_end = d["targets_end"]
+        sentiment = d["sentiment"]
+        orig_selected = d["orig_selected"]
+        orig_tweet = d["orig_tweet"]
+        targets_start = d["targets_start"]
+        targets_end = d["targets_end"]
+        offsets = d["offsets"]
+
+        ids = ids.to(device, dtype=torch.long)
+        token_type_ids = token_type_ids.to(device, dtype=torch.long)
+        mask = mask.to(device, dtype=torch.long)
+        targets_start = targets_start.to(device, dtype=torch.long)
+        targets_end = targets_end.to(device, dtype=torch.long)
+
+        model.zero_grad()
+        outputs_start, outputs_end = model(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids,
+        )
+        loss = loss_fn(outputs_start, outputs_end, targets_start, targets_end)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        outputs_start = torch.softmax(outputs_start, dim=1).cpu().detach().numpy()
+        outputs_end = torch.softmax(outputs_end, dim=1).cpu().detach().numpy()
+        jaccard_scores = []
+        for px, tweet in enumerate(orig_tweet):
+            selected_tweet = orig_selected[px]
+            tweet_sentiment = sentiment[px]
+            jaccard_score, _ = calculate_jaccard_score(
+                original_tweet=tweet,
+                target_string=selected_tweet,
+                sentiment_val=tweet_sentiment,
+                idx_start=np.argmax(outputs_start[px, :]),
+                idx_end=np.argmax(outputs_end[px, :]),
+                offsets=offsets[px]
+            )
+            jaccard_scores.append(jaccard_score)
+
+        jaccards.update(np.mean(jaccard_scores), ids.size(0))
+        losses.update(loss.item(), ids.size(0))
+        tk0.set_postfix(loss=losses.avg, jaccard=jaccards.avg)
+
+
+def eval_fn(data_loader, model, device):
+    model.eval()
+    losses = AverageMeter()
+    jaccards = AverageMeter()
+    
+    with torch.no_grad():
+        tk0 = tqdm(data_loader, total=len(data_loader))
+        for bi, d in enumerate(tk0):
+            ids = d["ids"]
+            token_type_ids = d["token_type_ids"]
+            mask = d["mask"]
+            sentiment = d["sentiment"]
+            orig_selected = d["orig_selected"]
+            orig_tweet = d["orig_tweet"]
+            targets_start = d["targets_start"]
+            targets_end = d["targets_end"]
+            offsets = d["offsets"].numpy()
+
+            ids = ids.to(device, dtype=torch.long)
+            token_type_ids = token_type_ids.to(device, dtype=torch.long)
+            mask = mask.to(device, dtype=torch.long)
+            targets_start = targets_start.to(device, dtype=torch.long)
+            targets_end = targets_end.to(device, dtype=torch.long)
+
+            outputs_start, outputs_end = model(
+                ids=ids,
+                mask=mask,
+                token_type_ids=token_type_ids
+            )
+            loss = loss_fn(outputs_start, outputs_end, targets_start, targets_end)
+            outputs_start = torch.softmax(outputs_start, dim=1).cpu().detach().numpy()
+            outputs_end = torch.softmax(outputs_end, dim=1).cpu().detach().numpy()
+            jaccard_scores = []
+            for px, tweet in enumerate(orig_tweet):
+                selected_tweet = orig_selected[px]
+                tweet_sentiment = sentiment[px]
+                jaccard_score, _ = calculate_jaccard_score(
+                    original_tweet=tweet,
+                    target_string=selected_tweet,
+                    sentiment_val=tweet_sentiment,
+                    idx_start=np.argmax(outputs_start[px, :]),
+                    idx_end=np.argmax(outputs_end[px, :]),
+                    offsets=offsets[px]
+                )
+                jaccard_scores.append(jaccard_score)
+
+            jaccards.update(np.mean(jaccard_scores), ids.size(0))
+            losses.update(loss.item(), ids.size(0))
+            tk0.set_postfix(loss=losses.avg, jaccard=jaccards.avg)
+    
+    print(f"Jaccard = {jaccards.avg}")
+    return jaccards.avg
+
+
+'''
 def train_fn(data_loader, model, optimizer, device, scheduler):
     model.train()
     losses = AverageMeter()
@@ -178,5 +325,5 @@ def eval_fn(data_loader, model, device):
     mean_jac = np.mean(jaccards)
 
     return mean_jac, val_outputs
-
+'''
 
