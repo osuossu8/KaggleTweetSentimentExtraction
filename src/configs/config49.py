6561724807dd07d16cb7faed90375445f786e545
osuossu8 <osuosuossu18@gmail.com>
2020-06-13 03:50:34 +0000

    exp49

---
+++ src/configs/config49.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import tokenizers
+
+sys.path.append("/usr/src/app/kaggle/tweet-sentiment-extraction")
+
+
+MAX_LEN = 96
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 16
+EPOCHS = 4
+NUM_FOLDS = 5
+ROBERTA_PATH = "inputs/roberta-base"
+
+TOKENIZER = tokenizers.ByteLevelBPETokenizer(
+    vocab_file=f"{ROBERTA_PATH}/vocab.json", 
+    merges_file=f"{ROBERTA_PATH}/merges.txt", 
+    lowercase=True,
+    add_prefix_space=True
+)
+
+INPUT_DIR = 'inputs'
+OUT_DIR = 'models'
+TRAIN_PATH = os.path.join(INPUT_DIR, "train.csv")
+TEST_PATH = os.path.join(INPUT_DIR, "test.csv")
+SAMPLE_PATH = os.path.join(INPUT_DIR, "sample_submission.csv")
+
+FOLD0_ONLY = False
+DEBUG = False
